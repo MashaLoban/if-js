0dffe69affd0165f57f9145691ae4f038226f37e
unknown <predel666@mail.ru>
2021-05-24 21:41:35 +0300

    fixed

--- main.js
+++ main.js
@@ -18,15 +18,15 @@ test = Boolean(test); //true
 console.log(test);
 
 //Task 3
-let arr = [2, 3, 5, 8];
-let newArr = 1;
+const arr = [2, 3, 5, 8];
+let result = 1;
 for (let i = 0; i < arr.length; i++) {
-    newArr = newArr * arr[i];
+    result = result * arr[i];
 }
-console.log('Ответ Task3:', newArr);
+console.log('Ответ Task3:', result);
 
 //Task4
-let arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
+const arr2 = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i < arr2.length; i++) {
     if (arr2[i] > 5 && arr2[i] < 10) {
         // debugger;
@@ -35,7 +35,7 @@ for (let i = 0; i < arr2.length; i++) {
 }
 
 //Task 5
-let arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
+const arr3 = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i < arr2.length; i++) {
     if (!(arr3[i] % 2) && arr3[i] !== 0) {
         console.log('Ответ Task5:', arr3[i]);
