7ba1952182c151d206d3499026b3544c6c362f65
unknown <predel666@mail.ru>
2021-06-07 23:08:18 +0300

    homework-5

--- main.js
+++ main.js
@@ -90,3 +90,65 @@ p.forEach((item) => {
     const painter = changeColorP();
     item.addEventListener('click', painter);
 });
+
+//lesson-5: Task1.1
+const date = '2020-12-27';
+
+const newDate = (oldDate) => {
+    return oldDate.split('-').reverse().join('.');
+} 
+
+console.log(newDate(date));
+
+//lesson-5: Task1.2
+const data = [
+    {
+      country: 'Russia',
+      city: 'Saint Petersburg',
+      hotel: 'Hotel Leopold',
+    },
+    {
+      country: 'Spain',
+      city: 'Santa Cruz de Tenerife',
+      hotel: 'Apartment Sunshine',
+    },
+    {
+      country: 'Slowakia',
+      city: 'Vysokie Tatry',
+      hotel: 'Villa Kunerad',
+    },
+    {
+      country: 'Germany',
+      city: 'Berlin',
+      hotel: 'Hostel Friendship',
+    },
+    {
+      country: 'Indonesia',
+      city: 'Bali',
+      hotel: 'Ubud Bali Resort&SPA',
+    },
+    {
+      country: 'Netherlands',
+      city: 'Rotterdam',
+      hotel: 'King Kong Hostel',
+    },
+    {
+      country: 'Marocco',
+      city: 'Ourika',
+      hotel: 'Rokoko Hotel',
+    },
+    {
+      country: 'Germany',
+      city: 'Berlin',
+      hotel: 'Hotel Rehberge Berlin Mitte',
+    },
+  ];
+
+  const seachItem = (str) => {
+      for (let i=0; i<data.length; i++) {
+    if ((data[i].country = str)||(data[i].city = str)||(data[i].hotel = str)) {
+        return `${data[i].country} ${data[i].city} ${data[i].hotel}`;
+          }
+      }
+  }
+  console.log(seachItem('Germany'));
