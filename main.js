3295cc6056e22c33a25a1af6a6a5f1e9e92bbf09
unknown <predel666@mail.ru>
2021-06-18 00:07:50 +0300

    without 3 task

--- main.js
+++ main.js
@@ -146,9 +146,210 @@ const data = [
 
   const seachItem = (str) => {
       for (let i=0; i<data.length; i++) {
-    if ((data[i].country = str)||(data[i].city = str)||(data[i].hotel = str)) {
+    if ((data[i].country === str)||(data[i].city === str)||(data[i].hotel === str)) {
         return `${data[i].country} ${data[i].city} ${data[i].hotel}`;
           }
       }
   }
   console.log(seachItem('Germany'));
+
+  
+//lesson-6: Task1.1
+
+const palindrome2 = (word) => {
+  if (word === word.split('').reverse().join('')) {
+      return true;
+  }
+  return false;
+}
+
+//lesson-6: Task1.2
+const hotels2 = [
+  {
+    name: 'Hotel Leopold',
+    city: 'Saint Petersburg',
+    country: 'Russia',
+  },
+  {
+    name: 'Apartment Sunshine',
+    city: 'Santa Cruz de Tenerife',
+    country: 'Spain',
+  },
+  {
+    name: 'Villa Kunerad',
+    city: 'Vysokie Tatry',
+    country: 'Slowakia',
+  },
+  {
+    name: 'Hostel Friendship',
+    city: 'Berlin',
+    country: 'Germany',
+  },
+  {
+    name: 'Radisson Blu Hotel',
+    city: 'Kyiv',
+    country: 'Ukraine',
+  },
+  {
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+  },
+  {
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Virgin Hotel',
+    city: 'Chicago',
+    country: 'USA',
+  },
+  {
+    name: 'Grand Beach Resort',
+    city: 'Dubai',
+    country: 'United Arab Emirates',
+  },
+  {
+    name: 'Shilla Stay',
+    city: 'Seoul',
+    country: 'South Korea',
+  },
+  {
+    name: 'San Firenze Suites',
+    city: 'Florence',
+    country: 'Italy',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Black Penny Villas',
+    city: 'BTDC, Nuca Dua',
+    country: 'Indonesia',
+  },
+  {
+    name: 'Koko Hotel',
+    city: 'Tokyo',
+    country: 'Japan',
+  },
+  {
+    name: 'Ramada Plaza',
+    city: 'Istanbul',
+    country: 'Turkey',
+  },
+  {
+    name: 'Waikiki Resort Hotel',
+    city: 'Hawaii',
+    country: 'USA',
+  },
+  {
+    name: 'Puro Hotel',
+    city: 'Krakow',
+    country: 'Poland',
+  },
+  {
+    name: 'Asma Suites',
+    city: 'Santorini',
+    country: 'Greece',
+  },
+  {
+    name: 'Cityden Apartments',
+    city: 'Amsterdam',
+    country: 'Netherlands',
+  },
+  {
+    name: 'Mandarin Oriental',
+    city: 'Miami',
+    country: 'USA',
+  },
+  {
+    name: 'Concept Terrace Hotel',
+    city: 'Rome',
+    country: 'Italy',
+  },
+  {
+    name: 'Ponta Mar Hotel',
+    city: 'Fortaleza',
+    country: 'Brazil',
+  },
+  {
+    name: 'Four Seasons Hotel',
+    city: 'Sydney',
+    country: 'Australia',
+  },
+  {
+    name: 'Le Meridien',
+    city: 'Nice',
+    country: 'France',
+  },
+  {
+    name: 'Apart Neptun',
+    city: 'Gdansk',
+    country: 'Poland',
+  },
+  {
+    name: 'Lux Isla',
+    city: 'Ibiza',
+    country: 'Spain',
+  },
+  {
+    name: 'Nox Hostel',
+    city: 'London',
+    country: 'UK',
+  },
+  {
+    name: 'Leonardo Vienna',
+    city: 'Vienna',
+    country: 'Austria',
+  },
+  {
+    name: 'Adagio Aparthotel',
+    city: 'Edinburgh',
+    country: 'UK',
+  },
+  {
+    name: 'Steigenberger Hotel',
+    city: 'Hamburg',
+    country: 'Germany',
+  },
+];
+
+const seachString2 = (str) => {
+  const arr2 = [];
+  hotels2.forEach((item) => {
+    const str2 = `${item.name}, ${item.city}, ${item.country}`;
+    if (str2.includes(str)) {
+      arr2.push(str2);
+    }
+  })
+  return arr2;
+}
+
+console.log(seachString2('USA'));
+
+//lesson-6: Task1.3
+
+//НЕ РАБОТАЕТ, подскажи, пожалуйста, как сделать, чтобы заработало
+
+const seachString2 = arrNew.map((item) => {
+  const result = {};
+    for (let key in arrNew) {
+      if (item[key].country == undefined) {
+        result+= `${item[key].country} , ${item[key].city}`;
+      } else {
+        result += `${item[key].city}`;
+      }
+      return result;
+
+    }
+})
+
+console.log(seachString2(hotels2));
