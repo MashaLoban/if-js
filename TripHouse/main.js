d18659aaca41dd838cf57cd77f0dbc32f2196134
unknown <predel666@mail.ru>
2021-08-17 15:04:46 +0300

    fixed

--- TripHouse/main.js
+++ TripHouse/main.js
@@ -1,32 +1,27 @@
 let url = 'https://fe-student-api.herokuapp.com/api/hotels/popular';
 
-async function getHotels(url){
-let data = await fetch(url)
-.then(response => response.json())
-.then(data => data)
-.catch(err => console.log(err))
-return data
+async function getHotels(url) {
+  let data = await fetch(url)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => console.log(err))
+  return data
 }
 
-  const divEl = document.querySelector(".wrapper__item_home");
+  const divEl = document.querySelector('.wrapper__item_home');
 
 async function createHotels(){
+  let data = await getHotels(url);
   data.forEach(element => {
-
-    const div = document.createElement("div");
-  
-    const img = document.createElement("img"); 
+  const div = document.createElement('div');
+    const img = document.createElement('img');
     img.setAttribute('src', element.imageUrl);
-  
     const a = document.createElement('a');
     a.innerHTML = `${element.name}`;
     a.href = "#";
     a.setAttribute('src', `#${element.name}`);
-  
-  
     const paragraph = document.createElement('p');
     paragraph.innerHTML = `${element.country}, ${element.city}`;
-  
     div.appendChild(img);
     div.appendChild(a);
     div.appendChild(paragraph);
